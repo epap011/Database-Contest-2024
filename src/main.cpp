11dda15763cbd3bc80179ff510b34fc370f9f19f
panoklis <csd3665@csd.uoc.gr>
2024-12-18 01:11:18 +0200

    Score analysis (necessary)

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,20 @@
 int main(int argc, char *argv[])
 {
     int initSize = 1000000; // Initial data size.
-    int opSize = 20000;    // Number of operations.
+    int opSize = 10000;    // Number of operations.
+    double SingleEqualScore = 0;
+    double SingleGreaterScore = 0;
+    double DoubleEqualScore = 0;
+    double DoubleGreaterScore = 0;
+    double DoubleGreaterEqualScore = 0;
     double score = 0;
+    int SingleEqualCnt = 0;
+    int SingleGreaterCnt = 0;
+    int DoubleEqualCnt = 0;
+    int DoubleGreaterCnt = 0;
+    int DoubleGreaterEqualCnt = 0;
     int cnt = 0;
+    std::string message = "Average score: ";
     
     auto start = std::chrono::high_resolution_clock::now();
     DataExecuterDemo dataExecuter(initSize - 1, opSize);
@@ -19,7 +30,7 @@ int main(int argc, char *argv[])
     auto stop1 = std::chrono::high_resolution_clock::now();
     auto duration1 = std::chrono::duration_cast<std::chrono::milliseconds>(stop1 - start1);
     std::cout << "Time taken by CEEngine: "
-         << duration1.count() << " milliseconds" << std::endl;
+         << duration1.count() << " milliseconds" << std::endl << std::endl;
 
     Action action = dataExecuter.getNextAction();
 
@@ -35,12 +46,37 @@ int main(int argc, char *argv[])
             double realAns = dataExecuter.answer(ans);
             score += realAns;
             cnt++;
+            if (action.quals.size() == 1 && action.quals[0].compareOp == EQUAL){
+                SingleEqualScore += realAns;
+                SingleEqualCnt++;
+            }
+            else if (action.quals.size() == 1 && action.quals[0].compareOp == GREATER){
+                SingleGreaterScore += realAns;
+                SingleGreaterCnt++;
+            }
+            else if (action.quals.size() == 2 && action.quals[0].compareOp == EQUAL && action.quals[1].compareOp == EQUAL){
+                DoubleEqualScore += realAns;
+                DoubleEqualCnt++;
+            }
+            else if (action.quals.size() == 2 && action.quals[0].compareOp == GREATER && action.quals[1].compareOp == GREATER){
+                DoubleGreaterScore += realAns;
+                DoubleGreaterCnt++;
+            }
+            else if (action.quals.size() == 2 && ((action.quals[0].compareOp == GREATER && action.quals[1].compareOp == EQUAL) || (action.quals[0].compareOp == EQUAL && action.quals[1].compareOp == GREATER))){
+                DoubleGreaterEqualScore += realAns;
+                DoubleGreaterEqualCnt++;
+            }
         }
         action = dataExecuter.getNextAction();
     }
-    std::cout << score / cnt << std::endl;
+    std::cout << message << score / cnt << std::endl << std::endl <<"Score analysis"<< std::endl << std::endl;
+    std::cout << "(25%)   {A,B} = X: " << SingleEqualScore / SingleEqualCnt << std::endl;
+    std::cout << "(25%)   {A,B} > X: " << SingleGreaterScore / SingleGreaterCnt << std::endl;
+    std::cout << "(12.5%) {A} = X, {B} = Y: " << DoubleEqualScore / DoubleEqualCnt << std::endl;
+    std::cout << "(12.5%) {A,B} > X, {B,A} > Y: " << DoubleGreaterScore / DoubleGreaterCnt << std::endl;
+    std::cout << "(%25)   {A,B} > X, {B,A} = Y: " << DoubleGreaterEqualScore / DoubleGreaterEqualCnt << std::endl;
     auto stop2 = std::chrono::high_resolution_clock::now();
     auto duration2 = std::chrono::duration_cast<std::chrono::milliseconds>(stop2 - start2);
-    std::cout << "Time taken by while-loop: "
+    std::cout << std::endl <<"Time taken by while-loop: "
          << duration2.count() << " milliseconds" << std::endl;
 }
